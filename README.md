f34765d9ff41f2699aa323bee0ccbf8907ce0353
Kevin <pykevin@gmail.com>
2018-01-13 16:59:44 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+<img src="https://kevinpy.com/Projects/Chrome-Extension/out/chrome-extension-screenshot.png" align="center" />
+<br /><br />
+# Chrome Extension Boilerplate
+Create your Wep App for Google Chrome.<br />
+<br />
+[![Version](https://img.shields.io/github/release/kevinpy/chrome-extension.svg)](https://github.com/kevinpy/chrome-extension/releases)
+[![dependencies Status](https://david-dm.org/kevinpy/chrome-extension/status.svg)](https://david-dm.org/kevinpy/chrome-extension)
+[![devDependencies Status](https://david-dm.org/kevinpy/chrome-extension/dev-status.svg)](https://david-dm.org/kevinpy/chrome-extension?type=dev)
+[![Donate](https://img.shields.io/badge/Donate-PayPal-green.svg)](paypal.me/kevinpy)
+[![Codacy Badge](https://api.codacy.com/project/badge/Grade/1447c0dff5fd49fe93e9c2141ded90e1)](https://www.codacy.com/app/pykevin/chrome-extension?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=KevinPy/chrome-extension&amp;utm_campaign=Badge_Grade)
+***
+**Author**: *Kevin Py*<br />
+**Website**: <https://kevinpy.com>
+***
+#### Installation
+```
+$ git clone https://github.com/KevinPy/APPNAME.git
+$ cd chrome-extension
+$ npm install
+```
+
+#### Chrome Installation
+- To load your app, bring up the apps and extensions management page by clicking the settings icon and choosing **Tools > Extensions**.<br />
+- Make sure the **Developer mode** checkbox has been selected.<br />
+- Click the **Load unpacked extension** button, navigate to your app's folder and click **OK**.
+
+
+#### Scripts
+`npm start` : Compile and launch the Web App
---
+++ gulpfile.babel.js
@@ -0,0 +1,68 @@
+'use strict';
+
+const DIR_DEV = 'app',
+    DIR_PROD = 'dist';
+
+import gulp from 'gulp';
+import sass from 'gulp-sass';
+import htmlmin from 'gulp-htmlmin';
+
+const browserSync = require('browser-sync').create(),
+    reload = browserSync.reload;
+
+gulp.task('img', () => {
+    return gulp.src([`${DIR_DEV}/assets/img/**/*`])
+               .pipe(gulp.dest(`${DIR_PROD}/assets/img`))
+               .pipe(reload({ stream: true }));
+});
+
+gulp.task('js', () => {
+    return gulp.src([`${DIR_DEV}/assets/js/**/*`])
+               .pipe(gulp.dest(`${DIR_PROD}/assets/js`))
+               .pipe(reload({ stream: true }));
+});
+
+gulp.task('sass', () => {
+    return gulp.src([`${DIR_DEV}/assets/scss/**/*.scss`])
+               .pipe(sass({
+                   //outputStyle: 'compressed'
+               }))
+               .pipe(gulp.dest(`${DIR_PROD}/assets/css`))
+               .pipe(reload({ stream: true }));
+});
+
+gulp.task('templates', () => {
+    return gulp.src(`${DIR_DEV}/*.html`)
+               .pipe(htmlmin({
+                   caseSensitive: true,
+                   collapseWhitespace: true,
+                   quoteCharacter: '\'',
+                   removeEmptyAttributes: true,
+                   removeScriptTypeAttributes: true,
+                   removeStyleLinkTypeAttributes: true
+               }))
+               .pipe(gulp.dest(`${DIR_PROD}`))
+               .pipe(reload({ stream: true }));
+});
+
+gulp.task('manifest', () => {
+    return gulp.src(`${DIR_DEV}/manifest.json`)
+               .pipe(gulp.dest(`${DIR_PROD}`));
+});
+
+gulp.task('assets', ['img', 'js', 'sass', 'templates', 'manifest']);
+
+gulp.task('default', ['assets'], () => {
+
+    browserSync.init({
+    server: {
+        baseDir: `./${DIR_PROD}`
+    }
+});
+
+gulp.watch(`${DIR_DEV}/assets/img/**/*`, ['img']);
+gulp.watch(`${DIR_DEV}/assets/js/**/*`, ['js']);
+gulp.watch(`${DIR_DEV}/assets/scss/**/*`, ['sass']);
+gulp.watch(`${DIR_DEV}/*.html`, ['templates']);
+
+});
