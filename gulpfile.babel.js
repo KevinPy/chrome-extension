8c89f05c30b4e8779e57f10dbbff4306cf50595f
Kevin <pykevin@gmail.com>
2018-01-14 21:51:44 +0100

    FIX lint

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,68 +1,68 @@
-'use strict';
+"use strict";
 
-const DIR_DEV = 'app',
-    DIR_PROD = 'dist';
+const DIR_DEV = "app",
+  DIR_PROD = "dist";
 
-import gulp from 'gulp';
-import sass from 'gulp-sass';
-import htmlmin from 'gulp-htmlmin';
+import gulp from "gulp";
+import sass from "gulp-sass";
+import htmlmin from "gulp-htmlmin";
 
-const browserSync = require('browser-sync').create(),
-    reload = browserSync.reload;
+const browserSync = require("browser-sync").create(),
+  reload = browserSync.reload;
 
-gulp.task('img', () => {
-    return gulp.src([`${DIR_DEV}/assets/img/**/*`])
-               .pipe(gulp.dest(`${DIR_PROD}/assets/img`))
-               .pipe(reload({ stream: true }));
+gulp.task("img", () => {
+  return gulp.src([`${DIR_DEV}/assets/img/**/*`])
+             .pipe(gulp.dest(`${DIR_PROD}/assets/img`))
+             .pipe(reload({ stream: true }));
 });
 
-gulp.task('js', () => {
-    return gulp.src([`${DIR_DEV}/assets/js/**/*`])
-               .pipe(gulp.dest(`${DIR_PROD}/assets/js`))
-               .pipe(reload({ stream: true }));
+gulp.task("js", () => {
+  return gulp.src([`${DIR_DEV}/assets/js/**/*`])
+             .pipe(gulp.dest(`${DIR_PROD}/assets/js`))
+             .pipe(reload({ stream: true }));
 });
 
-gulp.task('sass', () => {
-    return gulp.src([`${DIR_DEV}/assets/scss/**/*.scss`])
-               .pipe(sass({
-                   //outputStyle: 'compressed'
-               }))
-               .pipe(gulp.dest(`${DIR_PROD}/assets/css`))
-               .pipe(reload({ stream: true }));
+gulp.task("sass", () => {
+  return gulp.src([`${DIR_DEV}/assets/scss/**/*.scss`])
+             .pipe(sass({
+               //outputStyle: "compressed"
+             }))
+             .pipe(gulp.dest(`${DIR_PROD}/assets/css`))
+             .pipe(reload({ stream: true }));
 });
 
-gulp.task('templates', () => {
-    return gulp.src(`${DIR_DEV}/*.html`)
-               .pipe(htmlmin({
-                   caseSensitive: true,
-                   collapseWhitespace: true,
-                   quoteCharacter: '\'',
-                   removeEmptyAttributes: true,
-                   removeScriptTypeAttributes: true,
-                   removeStyleLinkTypeAttributes: true
-               }))
-               .pipe(gulp.dest(`${DIR_PROD}`))
-               .pipe(reload({ stream: true }));
+gulp.task("templates", () => {
+  return gulp.src(`${DIR_DEV}/*.html`)
+             .pipe(htmlmin({
+               caseSensitive: true,
+               collapseWhitespace: true,
+               quoteCharacter: "\"",
+               removeEmptyAttributes: true,
+               removeScriptTypeAttributes: true,
+               removeStyleLinkTypeAttributes: true
+             }))
+             .pipe(gulp.dest(`${DIR_PROD}`))
+             .pipe(reload({ stream: true }));
 });
 
-gulp.task('manifest', () => {
-    return gulp.src(`${DIR_DEV}/manifest.json`)
-               .pipe(gulp.dest(`${DIR_PROD}`));
+gulp.task("manifest", () => {
+  return gulp.src(`${DIR_DEV}/manifest.json`)
+             .pipe(gulp.dest(`${DIR_PROD}`));
 });
 
-gulp.task('assets', ['img', 'js', 'sass', 'templates', 'manifest']);
+gulp.task("assets", ["img", "js", "sass", "templates", "manifest"]);
 
-gulp.task('default', ['assets'], () => {
+gulp.task("default", ["assets"], () => {
 
-    browserSync.init({
+  browserSync.init({
     server: {
-        baseDir: `./${DIR_PROD}`
+      baseDir: `./${DIR_PROD}`
     }
-});
+  });
 
-gulp.watch(`${DIR_DEV}/assets/img/**/*`, ['img']);
-gulp.watch(`${DIR_DEV}/assets/js/**/*`, ['js']);
-gulp.watch(`${DIR_DEV}/assets/scss/**/*`, ['sass']);
-gulp.watch(`${DIR_DEV}/*.html`, ['templates']);
+  gulp.watch(`${DIR_DEV}/assets/img/**/*`, ["img"]);
+  gulp.watch(`${DIR_DEV}/assets/js/**/*`, ["js"]);
+  gulp.watch(`${DIR_DEV}/assets/scss/**/*`, ["sass"]);
+  gulp.watch(`${DIR_DEV}/*.html`, ["templates"]);
 
 });
